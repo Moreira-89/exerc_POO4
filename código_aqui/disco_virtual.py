007c42194c729bc8c9050e4042a7883964326b0c
Lucas Moreira <contatoparalucasmoreira@gmail.com>
2023-05-24 19:20:06 -0300

    Update disco_virtual.py

---
+++ código_aqui/disco_virtual.py
@@ -0,0 +1,22 @@
+class DiscoVirtual:
+    def __init__(self):
+        self.pastas = []
+
+    def criar_pasta(self, nome):
+        pasta = Pasta(nome)
+        self.pastas.append(pasta)
+
+    def remover_pasta(self, pasta):
+        self.pastas.remove(pasta)
+
+    def obter_pasta(self, nome):
+        for pasta in self.pastas:
+            if pasta.nome == nome:
+                return pasta
+        return None
+
+    def obter_tamanho_pasta(self, nome):
+        pasta = self.obter_pasta(nome)
+        if pasta:
+            return pasta.obter_tamanho_total()
+        return 0
